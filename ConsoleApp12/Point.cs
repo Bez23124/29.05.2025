59aced29fd39bd68baabca1357ae3a62f4c509c1
Bez23124 <157889944+Bez23124@users.noreply.github.com>
2025-05-30 12:30:55 +0300

    Add files via upload

---
+++ ConsoleApp12/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Point<T>
+    {
+        private T x;
+        private T y;
+
+        public Point(T x, T y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public T X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public T Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        public void Display()
+        {
+            Console.WriteLine($"X = {x}, Y = {y}");
+        }
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point<int> p1 = new Point<int>(3, 5);
+            p1.Display();
+
+            Point<uint> p2 = new Point<uint>(7, 9);
+            p2.Display();
+
+            Point<double> p3 = new Point<double>(3.5, 6.7);
+            p3.Display();
+            Console.ReadLine();
+        }
+    }
+}
